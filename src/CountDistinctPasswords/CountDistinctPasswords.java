e1a81343a012fb5a87cb388472c12ce76a01d3cc
qianqian-zhang <zhang.qianqia@northeastern.edu>
2024-03-10 19:29:56 -0700

    added descriptions

--- src/CountDistinctPasswords/CountDistinctPasswords.java
+++ src/CountDistinctPasswords/CountDistinctPasswords.java
@@ -1,26 +1,29 @@
 package CountDistinctPasswords;
+//given a string
+//you can reverse a substring and generate a new string
+//return number of unique strings that you can generate
 
+//1. don't reverse palindromic substrings
+//2. for a non-palindromic substring [a, b], if chars at a and b are equal AND substring[a + 1, b - 1] is palindromic, dont reverse
 public class CountDistinctPasswords {
     public long countDistinctPasswords(String password) {
-        // write your code here
+        //check substrings of length [2, n]
+
+        //find number of non-palindrome substrings
+        //start char != end char
+        //start char == end char, inner substring [start + 1, end - 1] is NOT palindromic
 
         int n = password.length();
-        long ans = 1l;
+        long ans = 1l; //count the original substring
 
-        //check substring of length [2, n - 1]
+        boolean[][] isPalindrome = new boolean[n][n]; //denotes if substring [i, j] is palindromic
 
-        //bruteforce
-
-        //find number of palindrome substrings of a string
-        //dp???
-
-        //abaa
-        boolean[][] isPalindrome = new boolean[n][n];
-
+        //substring of length 1
         for(int i = 0; i < n; i++) {
             isPalindrome[i][i] = true;
         }
 
+        //substring of length 2
         for(int i = 1; i < n; i++) {
             if(password.charAt(i) == password.charAt(i - 1)) {
                 isPalindrome[i - 1][i] = true;
@@ -42,16 +45,8 @@ public class CountDistinctPasswords {
                     }
                 }
             }
-
-
-
         }
 
-        //"abaa"
         return ans;
-
-        //len = 2: baaa aaba
-        //len = 3: aaab
-        //len = 4: aaba
 }
 }
